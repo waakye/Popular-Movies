3cb1433c20bc2fb8ca4c2e1afa86aa87b8555a2d
waakye <dailyomnivore@gmail.com>
2017-12-26 17:24:44 +0800

    Add MovieAdapter class with MovieViewHolder class

---
+++ app/src/main/java/com/waakye/android/popularmovies9/MovieAdapter.java
@@ -0,0 +1,45 @@
+package com.waakye.android.popularmovies9;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by lesterlie on 12/26/17.
+ */
+
+public class MovieAdapter {
+
+    /**
+     * Cache of the children views for a list item
+     */
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        // Will display the position in the list
+        TextView listItemMovieView;
+
+        /**
+         * Constructor for our ViewHolder.  Within this constructor, we get a reference to the
+         * TextViews and set an onClickListener to listen for clicks.  Those will be handled by the
+         * onClick method below
+         * @param itemView  The View that you inflated in
+         *                  {@link MovieAdapter#onCreateViewHolder(ViewGroup, int)}
+         */
+        public MovieViewHolder(View itemView){
+            super(itemView);
+
+            listItemMovieView = (TextView) itemView.findViewById(R.id.text_view_item_movie);
+        }
+
+        /**
+         * Create a convenience method that takes an integer as input and use that integer to
+         * display the appropriate text within a list item.
+         *
+         * @param listIndex Position of the item in the list
+         */
+        public void bind(int listIndex) {
+            listItemMovieView.setText(String.valueOf(listIndex));
+        }
+    }
+
+}

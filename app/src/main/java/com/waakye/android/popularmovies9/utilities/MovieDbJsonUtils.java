2ca9c4df261ffb4a470ea4d915f404fdf25c9b1d
waakye <dailyomnivore@gmail.com>
2017-12-27 10:51:05 +0800

    Add extractTrailerFromJson() method to MovieDbJsonUtils that returns a List of Trailer objects

--- app/src/main/java/com/waakye/android/popularmovies9/utilities/MovieDbJsonUtils.java
+++ app/src/main/java/com/waakye/android/popularmovies9/utilities/MovieDbJsonUtils.java
@@ -135,6 +135,49 @@ public class MovieDbJsonUtils {
         return parseTrailerData;
     }
 
+    /**
+     * This method parses JSON from a web response and returns a List of Trailers describing the
+     * trailers' charactertistics
+     *
+     * @param trailerJsonStr    JSON response from the server
+     *
+     * @return                  List of Trailers describing the trailer data
+     * @throws JSONException    If JSON data cannot be properly parsed
+     */
+    public static List<Trailer> extractTrailerFromJson(String trailerJsonStr) throws JSONException {
+
+        Log.i(LOG_TAG, "extractTrailerFromJson() method is called...");
+
+        // If the JSON string is empty or null, then return early
+        if (TextUtils.isEmpty(trailerJsonStr)) {
+            return null;
+        }
+
+        // Create an empty ArrayList that we can start adding movieListings to
+        ArrayList<Trailer> listOfTrailers = new ArrayList<>();
+
+        JSONObject baseTrailerJsonResponse = new JSONObject(trailerJsonStr);
+
+        JSONArray trailersArray = baseTrailerJsonResponse.getJSONArray("results");
+
+        for (int i = 0; i < trailersArray.length(); i++) {
+            // Get trailer JSONObject at position i
+            JSONObject currentTrailer = trailersArray.getJSONObject(i);
+
+            String trailerName = currentTrailer.getString("name");
+            String trailerType = currentTrailer.getString("type");
+            String trailerSite = currentTrailer.getString("site");
+            String trailerKey = currentTrailer.getString("key");
+
+            Trailer trailer = new Trailer(trailerName, trailerType, trailerSite, trailerKey);
+
+            listOfTrailers.add(trailer);
+
+        }
+        return listOfTrailers;
+    }
+
+
     // For this query: http://api.themoviedb.org/3/movie/83542/reviews?api_key=b2433ced24ee89f33371c184240eca2a
     public static String[] getUserReviewStringsFromJson(Context context, String reviewsJsonStr)
             throws JSONException{
@@ -167,12 +210,12 @@ public class MovieDbJsonUtils {
     }
 
     /**
-     * This method parses JSON from a web response and returns an array of Strings describing the
-     * movie possibilities.
+     * This method parses JSON from a web response and returns a List of MovieListing objects
+     * describing the movie possibilities.
      *
      * @param movieJsonStr  JSON response from the server
      *
-     * @return Array of Strings describing the movie data
+     * @return A List of MovieListing objects describing the movie data
      *
      * @throws org.json.JSONException  If JSON data cannot be properly parsed.
      */

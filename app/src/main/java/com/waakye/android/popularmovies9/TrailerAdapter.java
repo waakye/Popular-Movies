001a2025f184eb2e080703b58fe6bbac07a66087
waakye <dailyomnivore@gmail.com>
2017-12-27 10:06:28 +0800

    Add Adapter subclass methods onCreateViewHolder, onBindViewHolder, and getItemCount to TrailerAdapter

--- app/src/main/java/com/waakye/android/popularmovies9/TrailerAdapter.java
+++ app/src/main/java/com/waakye/android/popularmovies9/TrailerAdapter.java
@@ -1,14 +1,83 @@
 package com.waakye.android.popularmovies9;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
  * Created by lesterlie on 12/27/17.
  */
 
-public class TrailerAdapter {
+public class TrailerAdapter extends RecyclerView.Adapter<TrailerAdapter.TrailerViewHolder> {
+
+    public static final String LOG_TAG = TrailerAdapter.class.getSimpleName();
+
+    private int mNumberOfTrailerItems;
+
+    /**
+     * Constructor for TrailerAdapter that accepts a number of Trailer items to display and the
+     * specification for the ListItemClickListener
+     *
+     * @param numberOfTrailerItems    Number of movie items to display in list
+     */
+    public TrailerAdapter(int numberOfTrailerItems){ mNumberOfTrailerItems = numberOfTrailerItems;}
+
+
+    /**
+     * This gets called when each new ViewHolder is created.  This happens when the RecyclerView is
+     * laid out. Once enough ViewHolders are created to fill the screen, then this method doesn't
+     * need to be called anymore.
+     * @param viewGroup     The ViewGroup that these ViewHolders are contained within.
+     * @param viewType      If your RecyclerView has more than one type of item (which ours doesn't)
+     *                      you can use this viewType integer to provide a different layout.  See
+     *                      {@link android.support.v7.widget.RecyclerView.Adapter#getItemViewType(int)}
+     *                      for more details.
+     * @return              A new TrailerViewHolder that holds the View for each list item
+     */
+    @Override
+    public TrailerViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutForListItem = R.layout.movies_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutForListItem, viewGroup, shouldAttachToParentImmediately);
+        TrailerViewHolder viewHolder = new TrailerViewHolder(view);
+
+        return viewHolder;
+    }
+
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified position.
+     * In this method, we update the contents of the ViewHolder to display the correct indices in
+     * the list for this particular position, using the 'position' argument that is conveniently
+     * passed into us.
+     *
+     * @param holder    The ViewHolder which should be updated to represent the contents of the
+     *                  item at the given position in the data set.
+     * @param position  The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(TrailerViewHolder holder, int position) {
+        Log.d(LOG_TAG, "#" + position);
+        holder.bind(position);
+    }
+
+    /**
+     * This method simply returns the number of items to display.  It is used behind the scenes to
+     * help layout our Views and for animations.
+     *
+     * @return      The number of items available in our movie list
+     */
+    @Override
+    public int getItemCount() {
+        return mNumberOfTrailerItems;
+    }
+
 
     /**
      * Cache of the children views for a list item
@@ -41,5 +110,4 @@ public class TrailerAdapter {
             listItemMovieView.setText(String.valueOf(listIndex));
         }
     }
-
 }

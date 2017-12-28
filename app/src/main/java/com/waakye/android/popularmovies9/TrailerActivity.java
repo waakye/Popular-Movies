7bb66b634d353555e4315e1c6cceb76eed3a068b
waakye <dailyomnivore@gmail.com>
2017-12-28 10:51:58 +0800

    Add LoaderCallbacks including AsyncTaskLoader to TrailerActivity

--- app/src/main/java/com/waakye/android/popularmovies9/TrailerActivity.java
+++ app/src/main/java/com/waakye/android/popularmovies9/TrailerActivity.java
@@ -2,8 +2,10 @@ package com.waakye.android.popularmovies9;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,7 +13,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.waakye.android.popularmovies9.utilities.MovieDbJsonUtils;
 import com.waakye.android.popularmovies9.utilities.NetworkUtils;
@@ -27,7 +28,8 @@ import java.util.List;
  */
 
 public class TrailerActivity extends AppCompatActivity
-        implements TrailerListingAdapter.ListItemClickListener {
+    implements TrailerListingAdapter.ListItemClickListener,
+        LoaderManager.LoaderCallbacks<List<Trailer>>{
 
     public static String LOG_TAG = TrailerActivity.class.getSimpleName();
 
@@ -49,14 +51,7 @@ public class TrailerActivity extends AppCompatActivity
 
     protected List<Trailer> jsonTrailerDataList;
 
-//    protected String[] jsonTrailerData;
-//
-//    protected String[] simplerJsonTrailerData;
-
-    private Toast mToast;
-
     private static final int TRAILER_LOADER_ID = 11;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,8 +65,7 @@ public class TrailerActivity extends AppCompatActivity
 
         mTrailersList = (RecyclerView)findViewById(R.id.recycler_view_trailers);
 
-        mTrailerActivityErrorMessageDisplay =
-                (TextView)findViewById(R.id.trailer_activity_text_view_error_message_display);
+        mTrailerActivityErrorMessageDisplay = (TextView)findViewById(R.id.trailer_activity_text_view_error_message_display);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         mTrailersList.setLayoutManager(layoutManager);
@@ -82,16 +76,45 @@ public class TrailerActivity extends AppCompatActivity
 
         mAdapter = new TrailerListingAdapter(this, jsonTrailerDataList, this);
 
-        mTrailerActivityLoadingIndicator =
-                (ProgressBar) findViewById(R.id.trailer_activity_progress_bar_loading_indicator);
+        mTrailerActivityLoadingIndicator = (ProgressBar) findViewById(R.id.trailer_activity_progress_bar_loading_indicator);
 
-        makeTrailerQuery(movieId);
+        LoaderManager lm = getSupportLoaderManager();
+        lm.initLoader(TRAILER_LOADER_ID, null, this);
+
     }
 
     private void makeTrailerQuery(String movieId){
 
         Log.i(LOG_TAG, "makeTrailerQuery() method called...");
-        new TrailersQueryTask().execute(movieId);
+        /*
+         * This ID will uniquely identify the Loader. We can use it, for example, to get a handle
+         * on our Loader at a later point in time through the support LoaderManager.
+         */
+        int loaderId = TRAILER_LOADER_ID;
+
+        /*
+         * From MainActivity, we have implemented the LoaderCallbacks interface with the type of
+         * List of MovieListing objects. (implements LoaderCallbacks<List<MovieListing>>)
+         * The variable callback is passed to the call to initLoader below. This means that
+         * whenever the loaderManager has something to notify us of, it will do so through
+         * this callback.
+         */
+        LoaderManager.LoaderCallbacks<List<Trailer>> callback = TrailerActivity.this;
+
+        /*
+         * The second parameter of the initLoader method below is a Bundle. Optionally, you can
+         * pass a Bundle to initLoader that you can then access from within the onCreateLoader
+         * callback. In our case, we don't actually use the Bundle, but it's here in case we wanted
+         * to.
+         */
+        Bundle bundleForLoader = null;
+
+        /*
+         * Ensures a loader is initialized and active. If the loader doesn't already exist, one is
+         * created and (if the activity/fragment is currently started) starts the loader. Otherwise
+         * the last created loader is re-used.
+         */
+        getSupportLoaderManager().initLoader(loaderId, bundleForLoader, callback);
     }
 
     /**
@@ -129,7 +152,7 @@ public class TrailerActivity extends AppCompatActivity
      */
     @Override
     public void onListItemClick(int clickedItemIndex) {
-        Log.i(LOG_TAG, "onListItemClick() method called...");
+        Log.i(LOG_TAG, "onListItem() method called...");
 
         Trailer individualTrailer = jsonTrailerDataList.get(clickedItemIndex);
         String individualTrailerKey = individualTrailer.getTrailerKey();
@@ -140,71 +163,104 @@ public class TrailerActivity extends AppCompatActivity
         startActivity(i);
     }
 
-    public class TrailersQueryTask extends AsyncTask<String, Void, List<Trailer>> {
+    @Override
+    public Loader<List<Trailer>> onCreateLoader(int id, Bundle loaderArgs) {
 
-        // Override onPreExecute to set the loading indicator to visible
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            mTrailerActivityLoadingIndicator.setVisibility(View.VISIBLE);
-        }
+        return new AsyncTaskLoader<List<Trailer>>(this) {
 
-        @Override
-        protected List<Trailer> doInBackground(String... params) {
-            Log.i(LOG_TAG, "TrailersQueryTask doInBackground() method called...");
+            /* This List<MovieListing> will hold and help cache our MovieListing data */
+            List<Trailer> mTrailerListingData = null;
 
-            // If there's no search terms, then there's nothing to look up
-            if(params.length == 0){
-                return null;
+            /**
+             * Subclasses of AsyncTaskLoader must implement this to take care of their loading data
+             */
+            @Override
+            protected void onStartLoading(){
+                if(mTrailerListingData != null){
+                    deliverResult(mTrailerListingData);
+                } else {
+                    mTrailerActivityLoadingIndicator.setVisibility(View.VISIBLE);
+                    forceLoad();
+                }
             }
 
-            URL trailerSearchUrl = NetworkUtils.createMovieTrailerUrl(movieId);
+            /**
+             * This is the method of the AsyncTaskLoader that will load and parse the JSON data
+             * in the background.
+             *
+             * @return Trailer data from TheMovieDB as a List of Trailer objects;
+             *         null if an error occurs
+             */
+            @Override
+            public List<Trailer> loadInBackground(){
 
-            try {
-                String jsonTrailerResponse = NetworkUtils.getResponseFromHttpUrl(trailerSearchUrl);
+                URL trailerSearchUrl = NetworkUtils.createMovieTrailerUrl(movieId);
 
-                jsonTrailerDataList = MovieDbJsonUtils.extractTrailerFromJson(jsonTrailerResponse);
+                try {
+                    String jsonTrailerResponse = NetworkUtils.getResponseFromHttpUrl(trailerSearchUrl);
 
-                return jsonTrailerDataList;
+                    jsonTrailerDataList = MovieDbJsonUtils.extractTrailerFromJson(jsonTrailerResponse);
 
-            } catch (IOException e){
-                e.printStackTrace();
-                return null;
-            } catch (JSONException e){
-                e.printStackTrace();
-                return null;
+                    return jsonTrailerDataList;
+
+                } catch (IOException e){
+                    e.printStackTrace();
+                    return null;
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    return null;
+                }
             }
-        }
 
-        @Override
-        protected void onPostExecute(List<Trailer> trailersSearchResults){
-            Log.i(LOG_TAG, "TrailersQueryTask onPostExecute() method called...");
-            // As soon as the loading is complete, hide the loading indicator
-            mTrailerActivityLoadingIndicator.setVisibility(View.INVISIBLE);
-            if(trailersSearchResults != null && !trailersSearchResults.equals("")){
-                // Call showJsonDataView if we have valid, non-null results
-                showJsonDataView();
-
-                mAdapter.setTrailerData(trailersSearchResults);
-
-                mTrailersList.setAdapter(mAdapter);
-            } else {
-                // Call showErrorMessage if the result is null in onPostExecute
-                showErrorMessage();
+            /**
+             * Sends the result of the load to the registered listener.
+             *
+             * @param data The result of the load
+             */
+            public void deliverResult(List<Trailer> data) {
+                mTrailerListingData = data;
+                super.deliverResult(data);
             }
+        };
+
+    }
+    /**
+     * Called when a previously created loader has finished its load.
+     *
+     * @param loader The Loader that has finished.
+     * @param data The data generated by the Loader.
+     */
+    @Override
+    public void onLoadFinished(Loader<List<Trailer>> loader, List<Trailer> data) {
+        // As soon as the loading is complete, hide the loading indicator
+        mTrailerActivityLoadingIndicator.setVisibility(View.INVISIBLE);
+        if(data != null && !data.equals("")){
+            // Call showJsonDataView if we have valid, non-null results
+            showJsonDataView();
+            // Sets the RecyclerView Adapter, MovieAdapter, to the data source, a String array of movie urls
+            mAdapter.setTrailerData(data);
+            // RecyclerView's adapter is set to the RecyclerView Adapter
+            mTrailersList.setAdapter(mAdapter);
+
+        } else {
+            // Call showErrorMessage if the result is null in onPostExecute
+            showErrorMessage();
         }
     }
 
-//    public String[] extractTrailerNames (List<Trailer> trailers){
-//        Log.i(LOG_TAG, "extractTrailerNames() method callled...");
-//
-//        // Create a String[] array with a length equal to the size of the List of Movies
-//        String[] trailerNames = new String[trailers.size()];
-//        int index = 0;
-//        for(Trailer trailer : trailers){
-//            trailerNames[index] = (String) trailer.getTrailerName();
-//            index++;
-//        }
-//        return trailerNames;
-//    }
+    @Override
+    public void onLoaderReset(Loader<List<Trailer>> loader) {
+        /*
+         * We aren't using this method in our example application, but we are required to Override
+         * it to implement the LoaderCallbacks<String> interface
+         */
+    }
+
+    /**
+     * This method is used when we are resetting data, so that at one point in time during a
+     * refresh of our data, you can see that there is no data showing.
+     */
+    private void invalidateData() {
+        mAdapter.setTrailerData(null);
+    }
 }

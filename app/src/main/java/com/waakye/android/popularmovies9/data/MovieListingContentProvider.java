2b7c485874a627f1b4e33ce51a2dc72a46f8bd78
waakye <dailyomnivore@gmail.com>
2018-01-05 09:33:30 +0800

    Add MovieListingContentProvider class and register it in AndroidManifest.xml

---
+++ app/src/main/java/com/waakye/android/popularmovies9/data/MovieListingContentProvider.java
@@ -0,0 +1,54 @@
+package com.waakye.android.popularmovies9.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by lesterlie on 1/5/18.
+ */
+
+public class MovieListingContentProvider extends ContentProvider {
+
+    // Member variable for a MovieListingDbHelper that's initialized in the onCreate() method
+    private MovieListingDbHelper mMovieListingDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mMovieListingDbHelper = new MovieListingDbHelper(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}

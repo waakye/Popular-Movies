6b78689b7f9b3adef145f631f66bc0f7fa6306fb
waakye <dailyomnivore@gmail.com>
2018-01-08 23:42:47 +0800

    Add URIs to MovieListingContract class

--- app/src/main/java/com/waakye/android/popularmovies9/data/MovieListingContract.java
+++ app/src/main/java/com/waakye/android/popularmovies9/data/MovieListingContract.java
@@ -1,5 +1,6 @@
 package com.waakye.android.popularmovies9.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -12,9 +13,24 @@ public final class MovieListingContract {
     // Make the constructor private
     private MovieListingContract(){}
 
+    public static String MOVIE_POSTER_PREFIX = "https://image.tmdb.org/t/p/w500";
+
+    public static final String AUTHORITY = "com.waakye.android.popularmovies9";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    // Define the possible paths for accessing data in this contract
+    // This is the path for the "favorites" directory
+    public static final String PATH_FAVORITES = "favorites";
+
     /* Inner class that defines the table contents */
     public static class MovieListingEntry implements BaseColumns {
 
+        // MovieListingEntry content URI = base content URI + path
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITES).build();
+
+        // Favorites table and column names
         public static final String TABLE_NAME = "user_favorite_movies";
 
         public static final String COLUMN_MOVIE_TITLE = "movieTitle";

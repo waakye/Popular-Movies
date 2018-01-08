8b98d6a577d2e0af0bdc23122921d026755f064a
waakye <dailyomnivore@gmail.com>
2018-01-09 00:15:39 +0800

    Add UriMatcher() method to MovieListingContentProvider

--- app/src/main/java/com/waakye/android/popularmovies9/data/MovieListingContentProvider.java
+++ app/src/main/java/com/waakye/android/popularmovies9/data/MovieListingContentProvider.java
@@ -3,6 +3,7 @@ package com.waakye.android.popularmovies9.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -14,6 +15,36 @@ import android.support.annotation.Nullable;
 
 public class MovieListingContentProvider extends ContentProvider {
 
+    // Define the final integer constants for the directory of favorites and a single favorite movie
+    public static final int FAVORITES = 100;
+    public static final int FAVORITE_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    /**
+     * Define a static buildUriMatcher method that associates URI's with their int match
+     *
+     * Initialize a new matcher object without any matches, then use .addURI(String authority,
+     * String path, int match) to add matches
+     */
+    public static UriMatcher buildUriMatcher(){
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        /*
+          All paths added to the UriMatcher have a corresponding int.
+          For each kind of uri you may want to access, add the corresponding match with addURI.
+          The two calls below add matches for the favorites directory and a single item by ID.
+         */
+        uriMatcher.addURI(MovieListingContract.AUTHORITY, MovieListingContract.PATH_FAVORITES,
+                FAVORITES);
+        uriMatcher.addURI(MovieListingContract.AUTHORITY, MovieListingContract.PATH_FAVORITES
+                + "/#", FAVORITE_WITH_ID);
+
+        return uriMatcher;
+    }
+
     // Member variable for a MovieListingDbHelper that's initialized in the onCreate() method
     private MovieListingDbHelper mMovieListingDbHelper;
 
@@ -24,31 +55,36 @@ public class MovieListingContentProvider extends ContentProvider {
         return true;
     }
 
-    @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, String[] selectionArgs) {
+
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    @Nullable
-    @Override
-    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
-    }
-
-    @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
-    }
-
-    @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
-        return 0;
-    }
 }

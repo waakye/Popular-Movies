f36deb784ae10f68c8fcc237744b0c49d28728ab
waakye <dailyomnivore@gmail.com>
2018-01-09 20:21:13 +0800

    Make changes to CustomCursorAdapter based on the MovieListingAdapter

--- app/src/main/java/com/waakye/android/popularmovies9/CustomCursorAdapter.java
+++ app/src/main/java/com/waakye/android/popularmovies9/CustomCursorAdapter.java
@@ -13,29 +13,32 @@ import com.squareup.picasso.Picasso;
 import com.waakye.android.popularmovies9.data.MovieListingContract;
 import com.waakye.android.popularmovies9.data.MovieListingContract.MovieListingEntry;
 
-
 /**
+ * This CustomCursorAdapter creates and binds ViewHolders, that hold the poster path URL and
+ * the inflated image of the poster in a RecyclerView to efficiently display data
+ *
  * Created by lesterlie on 1/8/18.
  */
 
-public class CustomCursorAdapter extends
-        RecyclerView.Adapter<CustomCursorAdapter.FavoriteViewHolder>{
+public class CustomCursorAdapter extends RecyclerView.Adapter<CustomCursorAdapter.FavoriteViewHolder>{
 
     public static final String LOG_TAG = CustomCursorAdapter.class.getSimpleName();
 
-    // ImageView variable that refers to the ImageView on activity_main.xml
+    // ImageView variable that refers to the ImageView on movies_image_view_item.xml
     public ImageView listItemMovieImageView;
 
+    // Movie Poster Prefix that completes the url
+    public static String MOVIE_POSTER_PREFIX = "https://image.tmdb.org/t/p/w500";
 
-    // Class variables for the Cursor that holds the favorite movie data and the Contexxt
+    // Class variables for the Cursor that holds movie data and the Context
     private Cursor mCursor;
     private Context mContext;
 
     /*
-* An on-click handler that we've defined to make it easy for an Activity to interface with
-* our RecyclerView
-*/
-    final private CustomCursorAdapter.ListItemClickListener mOnClickListener;
+    * An on-click handler that we've defined to make it easy for an Activity to interface with
+    * our RecyclerView
+    */
+    final private ListItemClickListener mOnClickListener;
 
     /**
      * The interface that receives onClick messages.
@@ -45,108 +48,100 @@ public class CustomCursorAdapter extends
     }
 
     // Inner class for creating ViewHolders
-    public class FavoriteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        // Class variables for the favorite movies and TextViews
-        ImageView favoriteMovieImageView;
+    public class FavoriteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
-        /**
-         * Constructor for the FavoriteViewHolders
-         *
-         * @param itemView  The view inflated in onCreateViewHolder
-         */
         public FavoriteViewHolder(View itemView){
             super(itemView);
 
-            favoriteMovieImageView = (ImageView) itemView.findViewById(R.id.image_view_item_movie);
+            listItemMovieImageView = (ImageView)itemView.findViewById(R.id.image_view_item_movie);
 
-            // Call setOnClickListener on the View passed into the constructor (use 'this' as the OnClickListener)
+            // Call setOnClickListener on View passed into the constructor (use 'this' as the OnClickListener)
             itemView.setOnClickListener(this);
-
         }
 
-        /**
-         * Called whenever a user clicks on an item in the list.
-         * @param view The View that was clicked
-         */
         @Override
         public void onClick(View view) {
             int clickedPosition = getAdapterPosition();
             mOnClickListener.onListItemClick(clickedPosition);
-
         }
     }
-
 
     /**
      * Constructor for the CustomCursorAdapter that initializes the Context
      *
-     * @param context  the current Context
+     * @param context the current Context
      */
-    public CustomCursorAdapter(Context context, ListItemClickListener listener){
+    public CustomCursorAdapter(Context context, Cursor dataSet, ListItemClickListener listener){
+
         this.mContext = context;
+        mCursor = dataSet;
         mOnClickListener = listener;
     }
 
     /**
      * Called when ViewHolders are created to fill a RecyclerView
      *
-     * @return  A new FavoriteViewHolder that holds the view for each favorite movie
+     * @return A new FavoriteViewHolder that holds teh view for each favorite movie
      */
     @Override
-    public FavoriteViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+    public FavoriteViewHolder onCreateViewHolder(ViewGroup viewGroup,  int viewType){
 
-        // Inflate the movies_image_view_item.xml to a view
-        View view = LayoutInflater.from(mContext).inflate(R.layout.movies_image_view_item,
-                parent, false);
+        Context context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-        return new FavoriteViewHolder(view);
+        // FavoriteViewHolder created and connected to the movies_image_view_item.xml layout
+        int layoutForListItem = R.layout.movies_image_view_item;
+
+        boolean shouldAttachToParentImmediately = false;
+
+        // Create a new view
+        View view = inflater.inflate(layoutForListItem, viewGroup, shouldAttachToParentImmediately);
+        FavoriteViewHolder viewHolder = new FavoriteViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(CustomCursorAdapter.FavoriteViewHolder holder, int position) {
+    public void onBindViewHolder(FavoriteViewHolder holder, final int position) {
+        Log.i(LOG_TAG, "onBindViewHolder() method called with #" + position);
 
+        // Indices for the _id, poster path columns
         int idIndex = mCursor.getColumnIndex(MovieListingEntry._ID);
-        int titleIndex = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_TITLE);
-        int synopsisIndex = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_SYNOPSIS);
-        int posterPathIndex = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_POSTER_PATH);
-        int voteAverageIndex = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_VOTE_AVERAGE);
-        int releaseDateIndex = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_VOTE_AVERAGE);
-        int movieIdIndex = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_ID);
+        int posterPath = mCursor.getColumnIndex(MovieListingEntry.COLUMN_MOVIE_POSTER_PATH);
 
-        // get to the right location in the cursor
-        mCursor.moveToPosition(position);
+        mCursor.moveToPosition(position); // get to the right location in the cursor
 
-        String individualMoviePosterUrl = MovieListingContract.MOVIE_POSTER_PREFIX
-                + mCursor.getString(posterPathIndex);
+        // Determine the values of the desired data
+        final int id = mCursor.getInt(idIndex);
+        String poster_path = mCursor.getString(posterPath);
+        String individual_poster_url = MovieListingContract.MOVIE_POSTER_PREFIX
+                + poster_path;
 
-        Log.i(LOG_TAG, "moviePosterUrl: " + individualMoviePosterUrl);
-
-        // Uses the Picasso library and the movie Poster url to inflate the listItemMovieImageView
+        // Set values
         Picasso.with(mContext).setLoggingEnabled(true);
-        Picasso.with(mContext).load(individualMoviePosterUrl).into(listItemMovieImageView);
-
+        Picasso.with(mContext).load(individual_poster_url).into(listItemMovieImageView);
 
     }
 
     @Override
     public int getItemCount() {
-        if (mCursor == null){
+        if(mCursor == null){
             return 0;
         }
         return mCursor.getCount();
     }
 
     /**
-     * When data changes and re-query occurs, this function swaps the old Cursor with a newly
+     * When data changes and a re-query occurs, this functions swaps the old Cursor with a newly
      * updated Cursor (Cursor c) that is passed in.
      */
-    public Cursor swapCursor(Cursor c) {
-        // check if this cursor is the same as the previous cursor
+    public Cursor swapCursor(Cursor c){
+        // check if this cursor is the same as the previous cursor (mCursor)
         if (mCursor == c){
             return null; // because nothing has changed
         }
         Cursor temp = mCursor;
-        this.mCursor = c; // new cursor value assigned
+        this.mCursor = c;
 
         // check if this is a valid cursor, then update the cursor
         if (c != null){
@@ -154,4 +149,5 @@ public class CustomCursorAdapter extends
         }
         return temp;
     }
+
 }
